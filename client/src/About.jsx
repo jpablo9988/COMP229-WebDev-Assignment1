83ede34b59436e0cfb2dd13da15742398b248a25
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-01-28 23:05:16 -0500

    Moved pdf to assets menu

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -23,7 +23,7 @@ export default function About() {
                 This eventually developed into a passion for creating games. <br></br>
                 I love the process of it. I love coding, making art, making music and designing the levels. <br></br>
             </p>
-            <p> You can find my resume <a href = '../src/resources/cv.pdf'>here.</a> </p>
+            <p> You can find my resume <a href = '../src/assets/cv.pdf'>here.</a> </p>
             </body>
 
             
